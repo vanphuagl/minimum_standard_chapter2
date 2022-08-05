2790dcae83a99ad65629c584c9564db61d850db7
Phu Nguyen <nguyen_van_phu@allgrow-labo.jp>
2022-08-05 16:11:30 +0700

    update table reponsive

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -4,30 +4,30 @@
 let currentSlide = 0;
 let items = document.querySelectorAll(".p-design__img > img");
 
-const renderSlide = () => {
-    for (let i = 0; i < items.length; i++) {
-        if (i === currentSlide) {
-            items[i].classList.add("is-active");
-        }
+function renderSlide() {
+  for (let i = 0; i < items.length; i++) {
+    if (i === currentSlide) {
+      items[i].classList.add("is-active");
     }
-};
+  }
+}
 
-const clearSlide = () => {
-    for (let i = 0; i < items.length; i++) {
-        items[i].classList.remove("is-active");
-    }
-};
+function clearSlide() {
+  for (let i = 0; i < items.length; i++) {
+    items[i].classList.remove("is-active");
+  }
+}
 
 function nextSlide() {
-    currentSlide = currentSlide >= items.length - 1 ? 0 : currentSlide + 1;
-    clearSlide();
-    renderSlide();
+  currentSlide = currentSlide >= items.length - 1 ? 0 : currentSlide + 1;
+  clearSlide();
+  renderSlide();
 }
 
 function prevSlide() {
-    currentSlide = currentSlide <= 0 ? items.length - 1 : currentSlide - 1;
-    clearSlide();
-    renderSlide();
+  currentSlide = currentSlide <= 0 ? items.length - 1 : currentSlide - 1;
+  clearSlide();
+  renderSlide();
 }
 
 document.querySelector("#next").addEventListener("click", nextSlide);
